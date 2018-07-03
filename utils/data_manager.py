309af817eff136632f0241f39c092ba6ebbbd7df
Rahul <rahulhuilgol@gmail.com>
2018-07-03 16:48:22 -0700

    ebs data

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -1,7 +1,13 @@
 import os
 
 def getImagenetData(dataset):
-    if not os.path.exists(os.path.expanduser('/media/ramdisk/data/')):
-        os.system('mkdir -p /media/ramdisk/data/')
-        c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ /media/ramdisk/data/")
-        print('Downloaded data. Status %d'%c)
+	if 'ebs' in dataset:
+		if not os.path.exists(os.path.expanduser('~/data/')):
+	        os.system('mkdir -p ~/data/')
+	        c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ ~/data/")
+	        print('Downloaded data. Status %d'%c)
+    else:
+    	if not os.path.exists(os.path.expanduser('/media/ramdisk/data/')):
+	        os.system('mkdir -p /media/ramdisk/data/')
+	        c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ /media/ramdisk/data/")
+	        print('Downloaded data. Status %d'%c)
