c6220cedd715a0ba39a5800b967be30a7f6c5dd2
Rahul <rahulhuilgol@gmail.com>
2018-07-05 12:44:12 -0700

    data indent

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -3,11 +3,11 @@ import os
 def getImagenetData(dataset):
 	if 'ebs' in dataset:
 		if not os.path.exists(os.path.expanduser('~/data/')):
-	        os.system('mkdir -p ~/data/')
-	        c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ ~/data/")
-	        print('Downloaded data. Status %d'%c)
+			os.system('mkdir -p ~/data/')
+			c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ ~/data/")
+			print('Downloaded data. Status %d'%c)
     else:
-    	if not os.path.exists(os.path.expanduser('/media/ramdisk/data/')):
-	        os.system('mkdir -p /media/ramdisk/data/')
-	        c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ /media/ramdisk/data/")
-	        print('Downloaded data. Status %d'%c)
+		if not os.path.exists(os.path.expanduser('/media/ramdisk/data/')):
+			os.system('mkdir -p /media/ramdisk/data/')
+			c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ /media/ramdisk/data/")
+			print('Downloaded data. Status %d'%c)
