e63eb020f2b1aa742ea05320cbed382c6f3ac063
Rahul <rahulhuilgol@gmail.com>
2018-07-05 16:10:33 -0700

    indent fix

--- utils/data_manager.py
+++ utils/data_manager.py
@@ -6,7 +6,7 @@ def getImagenetData(dataset):
 			os.system('mkdir -p ~/data/')
 			c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ ~/data/")
 			print('Downloaded data. Status %d'%c)
-    else:
+	else:
 		if not os.path.exists(os.path.expanduser('/media/ramdisk/data/')):
 			os.system('mkdir -p /media/ramdisk/data/')
 			c = os.system("aws s3 sync s3://aws-ml-platform-datasets/imagenet/pass-through/ /media/ramdisk/data/")
